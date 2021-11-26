9a6f668c523572c36fc3560cd588994a4835015c
adarsh-pdgt <adarsh@primedigital.tech>
2021-11-26 10:21:03 +0530

    FEAT: basic setup for writing code

---
+++ social/settings/local.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+from .base import *
+
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+
+# ==============================================================================
+# EMAIL SETTINGS
+# ==============================================================================
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
